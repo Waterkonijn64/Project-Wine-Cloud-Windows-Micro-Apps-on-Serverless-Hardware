eb73019c68dfc75340372437954a6f85bc093ee1
G. <digiparrot@gmail.com>
2026-04-17 02:04:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# Start de VNC server op display :1
+
+# Zorg dat de VNC-lock bestanden weg zijn bij een herstart
+rm -rf /tmp/.X11-unix /tmp/.X*-lock
+
+# Start VNC server (display :1 is poort 5901)
 vncserver :1 -geometry 1280x800 -depth 16
 
-# Start de web-bridge (noVNC) op poort 7860
-# noVNC verbindt met de VNC server op poort 5901 (display :1)
+# Start noVNC bridge op poort 7860
+# In Ubuntu 22.04 staat noVNC in deze map:
 /usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 7860
-
